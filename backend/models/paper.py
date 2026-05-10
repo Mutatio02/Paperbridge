3c925956f59dc5f796e3b8dcb98b594c7637fe27
Mutatio02 <dragon55568@gmail.com>
2026-05-10 19:42:27 +0900

    change collected info

--- backend/models/paper.py
+++ backend/models/paper.py
@@ -1,12 +1,17 @@
 from pydantic import BaseModel
 from typing import Optional
-# 논문 기본 정보
+
 class PaperModel(BaseModel):
-    id: str             
-    title: str            
-    summary: str          
-    pdf_url: str           
-    published: str         
-    github_url: Optional[str] = None  
-    hf_url: Optional[str] = None      
+    id: str                 
+    title: str              
+    summary: str            
+    authors: Optional[str] = ""  
+    # [수정] 필수 필드에서 선택 필드로 변경
+    pdf_url: Optional[str] = "https://arxiv.org" 
+    published: str          
+    github_url: Optional[str] = None 
+    hf_url: Optional[str] = None     
+    source: str = "ArXiv"   
+    upvotes: int = 0
+    # [추가된 필드] ArXiv 수집 과정에서 임시로 GitHub 주소를 담아두는 용도
     temp_github_from_comment: Optional[str] = None
